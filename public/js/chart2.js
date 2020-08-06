07d2ebb3b620fb5fe48c34b2cc7d6785b0c6ab93
raouftams <raouftams@gmail.com>
2020-08-06 13:26:54 +0100

    06/08/2020

--- public/js/chart2.js
+++ public/js/chart2.js
@@ -18,8 +18,8 @@ $.ajax({url:"http://127.0.0.1:8000/dashboard/charts/data2",
                 datasets: [{
                     label: "Quantitées de DMA par mois",
                     data: data2,
-                    backgroundColor: ['rgba(105, 0, 132, .2)'],
-                    borderColor: ['rgba(200, 99, 132, .7)'],
+                    backgroundColor: ['#71c7ec'],
+                    borderColor: ['#107dac'],
                     borderWidth: 2
                     },]
                 },
